f6822d7c5a7dfd4b8e8145893e0e53656e0e0e78
Unprincess17 <shufan.gong@connect.um.edu.mo>
2025-02-23 06:47:32 -0700

    fix: redis setup earlier than populate resources

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ sudo apt install -y redis-server ffmpeg        && \
 sudo pip install redis flask pillow ffmpeg chameleon igraph  && \
 # compatible with python 3.8 and suitable for no-gpu and limited memory environment
 sudo pip install torch==1.9.1+cpu torchvision==0.10.1+cpu torchaudio==0.9.1 --index-url https://download.pytorch.org/whl/cpu && \
-python populate_resource.py             && \
 
 sudo redis-cli CONFIG SET requirepass "123456" && \
 sudo systemctl restart redis
+
+python populate_resource.py             && \
